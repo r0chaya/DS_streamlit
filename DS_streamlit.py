d193cf3059225987117755b375c3ed6ced1898b8
unknown <rochyatistat@gmail.com>
2025-02-17 20:59:43 +0700

    remove unuse import

--- DS_streamlit.py
+++ DS_streamlit.py
@@ -7,11 +7,11 @@ Created on Mon Feb 17 20:40:31 2025
 
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+#import seaborn as sns
+#import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+#from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import pickle
 import streamlit as st
 
